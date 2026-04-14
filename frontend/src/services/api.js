75eed473c68f14315a3464061a130a2e002e5f56
SupriyaDutta167 <dsupriya167@gmail.com>
2026-04-14 15:35:53 +0530

    push

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://seat-booking-system-22no.onrender.com/api",
+  baseURL: "http://locahost:5000/api",
 });
 
 // Automatically attach the user-id to headers for Role-Based Access
